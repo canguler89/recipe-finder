44a1a3ab502f8f8c61feaede06b84026885d2dce
Can Guler <canmuhammetguler@gmail.com>
2020-10-27 19:33:15 -0500

    starting up project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, { Component } from "react";
-import SearchRecipes from "./components/SearchRecipes";
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Recipe Finder</h1>
-        <SearchRecipes />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
 }
 
 export default App;
